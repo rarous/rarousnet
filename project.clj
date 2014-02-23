f208e81f5900dbdc0973800179495beb343f7b58
Aleš Roubíček <ales@roubicek.name>
2014-02-23 15:26:48 +0100

    Updated dependencies

--- project.clj
+++ project.clj
@@ -3,17 +3,18 @@
   :url "http://rarousnet.herokuapp.com"
   :license {:name "FIXME: choose"
             :url "http://example.com/FIXME"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure "1.1.1"]
-                 [ring/ring-jetty-adapter "1.1.0"]
-                 [ring/ring-devel "1.1.0"]
-                 [ring-basic-authentication "1.0.1"]
-                 [environ "0.2.1"]
+  :dependencies [[org.clojure/clojure "1.6.0-beta1"]
+                 [compojure "1.1.6"]
+                 [ring "1.2.1"]
+                 [ring-basic-authentication "1.0.5"]
+                 [environ "0.4.0"]
                  [com.cemerick/drawbridge "0.0.6"]
                  [enlive "1.1.5"]]
   :min-lein-version "2.0.0"
-  :plugins [[environ/environ.lein "0.2.1"]]
+  :plugins [[lein-environ "0.4.0"]]
   :hooks [environ.leiningen.hooks]
   :resource-paths ["src/rarousnet" "resources"]
-  :profiles {:production {:env {:production true}}}
+  :profiles
+    {:production
+     {:env {:production true}}}
   :main rarousnet.web)
