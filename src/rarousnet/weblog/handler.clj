97e019f49f3ab22f95d100dcd64b1d79fd207c1e
Aleš Roubíček <ales@roubicek.name>
2014-03-19 14:53:39 +0100

    fixed rubrika redirect

--- src/rarousnet/weblog/handler.clj
+++ src/rarousnet/weblog/handler.clj
@@ -93,7 +93,7 @@
   (GET "/ws/Syndikace.asmx/Rss" [] (redirect-to-rss-feed))
   (GET "/ws/syndikace.asmx/Rss" [] (redirect-to-rss-feed))
   (GET "/clanek/:url" [url] (redirect-to-blogpost url))
-  (GET "/rubrika/:url" [url] (redirect-to-blogpost (subs url (.indexOf url "-"))))
+  (GET "/rubrika/:url" [url] (redirect-to-blogpost (subs url (+ 1 (.indexOf url "-")))))
   (GET "/weblog" [] (redirect-to-blogpost ""))
   (GET "/weblog/429-test-driven-developemt-pribeh-nezbedneho-vyvojare.aspx" []
        (moved-permanently "/weblog/429-test-driven-development-pribeh-nezbedneho-vyvojare.aspx")))
