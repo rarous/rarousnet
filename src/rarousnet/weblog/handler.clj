5f3afc97063d8e4007e4e8cb5e613ca16eaeb0f5
Aleš Roubíček <ales@roubicek.name>
2014-07-09 15:52:16 +0200

    Category listing shows dates of publication

--- src/rarousnet/weblog/handler.clj
+++ src/rarousnet/weblog/handler.clj
@@ -1,10 +1,10 @@
 (ns rarousnet.weblog.handler
-  (:use net.cgrand.enlive-html
-        clj-time.coerce
-        clj-time.format
-        clj-time.local)
-  (:require [clojure.java.io :as io]
+  (:require [clj-time.core :as t]
+            [clj-time.coerce :refer [from-date]]
+            [clj-time.format :refer [formatter unparse with-locale formatters]]
+            [clojure.java.io :as io]
             [compojure.core :refer [defroutes GET]]
+            [net.cgrand.enlive-html :refer [defsnippet deftemplate] :as html]
             [ring.util.response :refer [charset]]))
 
 (def blog-url "http://www.rarous.net/weblog/")
@@ -12,95 +12,114 @@
 (def articles (read-resource "articles.edn"))
 (def categories (read-resource "rubrics.edn"))
 (defn load-article [url] (get articles (keyword url)))
-(defn load-category [url] (get categories (keyword url)))
 (defn last-articles [n]
   (->> (vals articles)
-
        (sort-by :id)
        (reverse)
        (take n)))
+(defn load-category [url]
+  (let [category-items (-> (get categories (keyword url)) reverse)
+        title (:category (first category-items))
+        years (->> category-items
+                   (map #(assoc % :year (t/year (from-date (:published %)))))
+                   (group-by :year)
+                   (mapv (fn [x] {:year (first x) :articles (second x)}))
+                   (sort-by :year)
+                   reverse
+                   (into []))]
+    {:title title, :url url, :years years}))
 
 (def long-date-format
   (with-locale
     (formatter "HH.mm - d. MMMM yyyy")
-    (new java.util.Locale "cs")))
+    (java.util.Locale. "cs")))
+(def short-date-format
+  (with-locale
+    (formatter "MMM d")
+    (java.util.Locale. "cs")))
 (def utc-format (formatters :basic-date-time))
 (def rss-format (formatter "EEE, d MMM yyyy HH:mm:ss Z"))
-(defn utc-date [article]
-  (->> (get article :published)
-       from-date
-       (unparse utc-format)))
-(defn long-date [article]
-  (->> (get article :published)
-       from-date
-       (unparse long-date-format)))
-(defn rss-date [article]
-  (->> (get article :published)
-       from-date
-       (unparse rss-format)))
+(defn utc-date [published]
+  (unparse utc-format (from-date published)))
+(defn long-date [published]
+  (unparse long-date-format (from-date published)))
+(defn short-date [published]
+  (unparse short-date-format (from-date published)))
+(defn rss-date [published]
+  (unparse rss-format (from-date published)))
 
-(defn permalink [article]
-  (str blog-url (article :id) "-" (article :url) ".aspx"))
-(defn author-twitter [article]
-  (let [author (article :author)]
-    (case author
-      "Aleš Roubíček" "@alesroubicek"
-      "Alessio Busta" "@alessiobusta"
-      nil)))
+(defn permalink [{:keys [id url]}]
+  (str blog-url id "-" url ".aspx"))
+(defn author-twitter [{:keys [author]}]
+  (case author
+    "Aleš Roubíček" "@alesroubicek"
+    "Alessio Busta" "@alessiobusta"
+    nil))
 
-(defn- meta-n [name] [:meta (attr= :name name)])
-(defn- meta-p [name] [:meta (attr= :property name)])
-(defn- itemprop [name] (attr= :itemprop name))
-(defn- link [rel] [:link (attr= :rel rel)])
+(defn- meta-n [name] [:meta (html/attr= :name name)])
+(defn- meta-p [name] [:meta (html/attr= :property name)])
+(defn- itemprop [name] (html/attr= :itemprop name))
+(defn- link [rel] [:link (html/attr= :rel rel)])
 
 (deftemplate index-template "weblog/index.html" [articles]
-  [:#content :article] (clone-for [article articles]
-                                  [:article :header (itemprop "name") :a] (content (article :title))
-                                  [:article :header (itemprop "name") :a] (set-attr :href (permalink article))
-                                  [:article (itemprop "articleBody")] (html-content (article :html))
-                                  [:article (itemprop "articleSection")] (content (article :category))
-                                  [:article (itemprop "author") (itemprop "name")] (content (article :author))
-                                  [:article (itemprop "url")] (set-attr :href (permalink article))
-                                  [:article (itemprop "datePublished")] (content (long-date article))
-                                  [:article (itemprop "datePublished")] (set-attr :datetime (utc-date article))))
+  [:#content :article] (html/clone-for [{:keys [title html category author published] :as article} articles]
+                                       [:article :header (itemprop "name") :a] (html/content title)
+                                       [:article :header (itemprop "name") :a] (html/set-attr :href (permalink article))
+                                       [:article (itemprop "articleBody")] (html/html-content html)
+                                       [:article (itemprop "articleSection")] (html/content category)
+                                       [:article (itemprop "author") (itemprop "name")] (html/content author)
+                                       [:article (itemprop "url")] (html/set-attr :href (permalink article))
+                                       [:article (itemprop "datePublished")] (html/content (long-date published))
+                                       [:article (itemprop "datePublished")] (html/set-attr :datetime (utc-date published))))
 
 (deftemplate rss-template "weblog/index.rss" [articles]
-  [:item] (clone-for [article articles]
-                     [:author] (content (article :author))
-                     [:title] (content (article :title))
-                     [:guid] (content (permalink article))
-                     [:link] (content (permalink article))
-                     [:pubDate] (content (rss-date article))
-                     [:description] (content (article :description))
-                     [:category] (content (article :category))))
+  [:item] (html/clone-for [{:keys [author title description category published] :as article} articles]
+                          [:author] (html/content author)
+                          [:title] (html/content title)
+                          [:guid] (html/content (permalink article))
+                          [:link] (html/content (permalink article))
+                          [:pubDate] (html/content (rss-date published))
+                          [:description] (html/content description)
+                          [:category] (html/content category)))
 
 (deftemplate comments-rss-template "weblog/comments.rss" [])
-(deftemplate blogpost-template "weblog/blogpost.html" [article]
-  [:title] (content (article :title))
-  [(meta-n "author")] (set-attr :content (article :author))
-  [(meta-n "description")] (set-attr :content (article :description))
-  [(meta-n "twitter:creator")] (set-attr :content (author-twitter article))
-  [(meta-n "twitter:title")] (set-attr :content (article :title))
-  [(meta-n "twitter:description")] (set-attr :content (article :description))
-  [(meta-p "article:published_time")] (set-attr :content (utc-date article))
-  [(meta-p "article:section")] (set-attr :content (article :category))
-  [(link "canonical")] (set-attr :href (permalink article))
-  [(link "category")] (set-attr :href (article :category-url))
-  [:article :header (itemprop "name")] (content (article :title))
-  [:article (itemprop "articleBody")] (html-content (article :html))
-  [:article (itemprop "articleSection")] (content (article :category))
-  [:article (itemprop "author") (itemprop "name")] (content (article :author))
-  [:article (itemprop "url")] (set-attr :href (permalink article))
-  [:article (itemprop "datePublished")] (content (long-date article))
-  [:article (itemprop "datePublished")] (set-attr :datetime (utc-date article)))
+(deftemplate blogpost-template "weblog/blogpost.html"
+  [{:keys [title author description category category-url html published] :as article}]
+  [:title] (html/content title)
+  [(meta-n "author")] (html/set-attr :content author)
+  [(meta-n "description")] (html/set-attr :content description)
+  [(meta-n "twitter:creator")] (html/set-attr :content (author-twitter article))
+  [(meta-n "twitter:title")] (html/set-attr :content title)
+  [(meta-n "twitter:description")] (html/set-attr :content description)
+  [(meta-p "article:published_time")] (html/set-attr :content (utc-date published))
+  [(meta-p "article:section")] (html/set-attr :content category)
+  [(link "canonical")] (html/set-attr :href (permalink article))
+  [(link "category")] (html/set-attr :href category-url)
+  [:article :header (itemprop "name")] (html/content title)
+  [:article (itemprop "articleBody")] (html/html-content html)
+  [:article (itemprop "articleSection")] (html/content category)
+  [:article (itemprop "author") (itemprop "name")] (html/content author)
+  [:article (itemprop "url")] (html/set-attr :href (permalink article))
+  [:article (itemprop "datePublished")] (html/content (long-date article))
+  [:article (itemprop "datePublished")] (html/set-attr :datetime (utc-date published)))
 
-(deftemplate category-template "weblog/category.html" [articles]
-  [:title] (content (str "Rubrika " (:category (first articles))))
-  [(link "canonical")] (set-attr :href (str blog-url (:category-url (first articles))))
-  [:#content :h2] (content (:category (first articles)))
-  [:#content :ul [:li first-of-type]] (clone-for [{:keys [title url id]} articles]
-                                                 [:li :a] (content title)
-                                                 [:li :a] (set-attr :href (permalink {:url url :id id}))))
+(defsnippet year-items "weblog/category.html" [:#content :section] [years]
+  [:section] (html/clone-for [{:keys [year articles]} years]
+                             [:section :h1] (html/content (str year))
+                             [:article] (html/substitute (category-items articles))))
+
+(defsnippet category-items "weblog/category.html" [:#content :article] [articles]
+  [:article] (html/clone-for [{:keys [title published] :as article} articles]
+                             [:article :a] (html/content title)
+                             [:article :a] (html/set-attr :href (permalink article))
+                             [:article :time] (html/content (short-date published))
+                             [:article :time] (html/set-attr :datetime (utc-date published))))
+
+(deftemplate category-template "weblog/category.html" [{:keys [title url years]}]
+  [:title] (html/content (str "Rubrika " title))
+  [(link "canonical")] (html/set-attr :href (str blog-url url))
+  [:#content :h2] (html/content title)
+  [:#content :section] (html/substitute (year-items years)))
 
 (defn render-view [template]
   (-> {:status 200
@@ -144,7 +163,6 @@
 
 (defn category [url]
   (some-> (load-category url)
-          reverse
           category-template
           render-view))
 
