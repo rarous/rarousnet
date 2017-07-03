aa42a5fe68eee379232240d3256a7f29196118e7
Aleš Roubíček <ales@roubicek.name>
2017-07-03 20:06:33 +0200

    Added inline scripts hashes

--- src/rarousnet/home/handler.clj
+++ src/rarousnet/home/handler.clj
@@ -9,11 +9,15 @@
 (deftemplate photos-template "home/photos.html" [])
 
 (defn render [template]
-  (-> {:status 200
-       :headers {"Content-Type" "text/html"
-                 "Content-Security-Policy" "default-src 'none'; font-src https://fonts.gstatic.com; img-src 'self' https://chart.googleapis.com https://res.cloudinary.com https://s.gravatar.com https://ssl.google-analytics.com https://www.google-analytics.com; script-src https://ajax.cloudflare.com/ https://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js https://ssl.google-analytics.com/ga.js https://www.google-analytics.com/analytics.js; style-src 'self' https://fonts.googleapis.com/"}
-       :body (apply str template)}
-      (charset "UTF-8")))
+  (let [font-src "font-src https://fonts.gstatic.com;"
+        img-src "img-src 'self' https://chart.googleapis.com https://res.cloudinary.com https://s.gravatar.com https://ssl.google-analytics.com https://www.google-analytics.com;"
+        script-src "script-src 'sha256-br0y5ghdHrZF9nYs7+rhDI2ifwAskUqeBxJRXxBx8VA=' 'sha256-aZf9oVm98J7yadFHTX5FX3aNX6HOESbl0sqKntOI1Ug=' 'sha256-GD/grf6BlxHnrvBIebfqE++izloaTRvCIWzO0z/VdSg=' 'sha256-lUNepec9kVSprWoNOZPpKsPttrd/p784gwgTMtQ1Aj8=' https://ajax.cloudflare.com/ https://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js https://ssl.google-analytics.com/ga.js https://www.google-analytics.com/analytics.js;"
+        style-src "style-src 'self' 'sha256-2vvYFkalq5E6kyFDpqXsNAWFB/oMxi5lptRfTndg7pI=' https://fonts.googleapis.com/"]
+    (-> {:status 200
+         :headers {"Content-Type" "text/html"
+                   "Content-Security-Policy" (str "default-src 'none';" font-src img-src script-src style-src)}
+         :body (apply str template)}
+        (charset "UTF-8"))))
 
 (defn moved-permanently [location]
   {:status 301
