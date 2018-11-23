bd97e3377a143ce28b57a21bc0824b1c2fd4574e
Aleš Roubíček <ales@roubicek.name>
2018-11-23 14:13:18 +0100

    Fixed tests

--- test/rarousnet/weblog/handler_test.clj
+++ test/rarousnet/weblog/handler_test.clj
@@ -1,39 +1,48 @@
 (ns rarousnet.weblog.handler-test
-  (:require [clojure.test :refer [testing is are]]
+  (:require [clojure.test :refer [testing is are deftest]]
             [rarousnet.weblog.handler :as sut]))
 
-(testing "Should create absolute url permalink"
-  (is (= (sut/permalink {:id 1 :url "test"})
-        "http://www.rarous.net/weblog/1-test.aspx")))
+(deftest permalink
+  (testing "Should create absolute url permalink"
+    (is (= (sut/permalink {:id 1 :url "test"})
+           "https://www.rarous.net/weblog/1-test.aspx"))))
 
-(testing "Should create relative url link"
-  (is (= (sut/rel-link {:id 1 :url "test"})
-        "/weblog/1-test.aspx")))
+(deftest rel-link
+  (testing "Should create relative url link"
+    (is (= (sut/rel-link {:id 1 :url "test"})
+           "/weblog/1-test.aspx"))))
 
-(testing "Should convert author name to twitter handle"
-  (is (= (sut/author-twitter {:author "Aleš Roubíček"})
-        "@alesroubicek"))
-  (is (= (sut/author-twitter {:author "Alessio Busta"})
-        "@alessiobusta"))
-  (is (= (sut/author-twitter {:author "Other"})
-        nil)))
+(deftest author-twitter
+  (testing "Should convert author name to twitter handle"
+    (is (= (sut/author-twitter {:author "Aleš Roubíček"})
+           "@alesroubicek"))
+    (is (= (sut/author-twitter {:author "Alessio Busta"})
+           "@alessiobusta"))
+    (is (= (sut/author-twitter {:author "Other"})
+           nil))))
 
-(testing "Should get category listing"
-  (is (= (sut/category "" {})
-        nil))
-  (is (= (:status (sut/category "skola.aspx" {}))
-        200)))
+(deftest categhory
+  (testing "Should get category listing"
+    (is (= (:status (sut/category "skola.aspx" {}))
+           200)))
+  (testing "Should not render unknown category"
+    (is (= (sut/category "" {})
+           nil))))
 
-(testing "Should render index"
-  (is (= (:status (sut/index {}))
-        200)))
+(deftest index
+  (testing "Should render index"
+    (is (= (:status (sut/index {}))
+           200))))
 
-(testing "Should render article"
-  (is (= (sut/blogpost "" {})
-        nil))
-  (is (= (:status (sut/blogpost "1-vitejte.aspx" {}))
-        200)))
+(deftest blogpost
+  (testing "Should render article"
+    (is (= (:status (sut/blogpost "1-vitejte.aspx" {}))
+           200)))
+  (testing "Should not render unknown article"
+    (is (= (sut/blogpost "" {})
+           nil))))
 
-(testing "Sould render RSS feed"
-  (is (= (:status (sut/rss))
-        200)))
+(deftest rss
+  (testing "Should render RSS feed"
+    (is (= (:status (sut/rss))
+           200))))
