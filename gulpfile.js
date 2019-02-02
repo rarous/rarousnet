8481504c6b678a6ea12d04635b70f289c73250f7
Aleš Roubíček <ales@roubicek.name>
2019-02-02 17:08:58 +0100

    HTML minimalization

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 const { src, dest, parallel, series, watch } = require("gulp");
 const hash = require("gulp-hash");
 const references = require("gulp-hash-references");
+const htmlMin = require("gulp-htmlmin");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const browserSync = require("browser-sync");
@@ -27,9 +28,22 @@ function hashStyles() {
     .pipe(dest("./out"));
 }
 
-function updateReferences() {
+function html() {
   return src("./dist/**/*.html")
     .pipe(references("./out/assets-manifest.json"))
+    .pipe(
+      htmlMin({
+        collapseBooleanAttributes: true,
+        collapseWhitespace: true,
+        minifyJS: true,
+        removeAttributeQuotes: true,
+        removeComments: true,
+        removeOptionalTags: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true
+      })
+    )
     .pipe(dest("./dist"));
 }
 
@@ -43,4 +57,4 @@ function run() {
 }
 
 exports.dev = parallel(css, run);
-exports.default = series(css, hashStyles, updateReferences);
+exports.default = series(css, hashStyles, html);
