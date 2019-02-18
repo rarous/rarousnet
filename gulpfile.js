5593ad2d8c3da0a8b1b8de8d53f865ee67e6eba5
Aleš Roubíček <ales@roubicek.name>
2019-02-18 13:37:07 +0100

    Improved Gulp pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 "use strict";
 
 const { src, dest, parallel, series, watch } = require("gulp");
-const hash = require("gulp-hash");
-const references = require("gulp-hash-references");
+const hash = require("gulp-rev");
+const references = require("gulp-rev-replace");
+const deleteNotHashed = require("gulp-rev-delete-original");
 const htmlMin = require("gulp-htmlmin");
+const notify = require("gulp-notify");
+const plumber = require("gulp-plumber");
 const postcss = require("gulp-postcss");
+const sizereport = require("gulp-sizereport");
 const autoprefixer = require("autoprefixer");
 const browserSync = require("browser-sync");
 const cssnano = require("cssnano");
@@ -16,20 +20,29 @@ function css() {
   ];
   return src("./static/**/*.css")
     .pipe(postcss(plugins))
+    .pipe(
+      plumber({ errorHandler: notify.onError("Error: <%= error.message %>") })
+    )
     .pipe(dest("./dist/"));
 }
 
 function hashStyles() {
   return src("./dist/**/*.css")
     .pipe(hash())
+    .pipe(deleteNotHashed())
     .pipe(dest("./dist"))
-    .pipe(hash.manifest("assets-manifest.json"))
+    .pipe(
+      hash.manifest("assets-manifest.json", {
+        merge: true
+      })
+    )
     .pipe(dest("./out"));
 }
 
 function html() {
+  const manifest = src("./out/assets-manifest.json");
   return src("./dist/**/*.html")
-    .pipe(references("./out/assets-manifest.json"))
+    .pipe(references({ manifest }))
     .pipe(
       htmlMin({
         collapseBooleanAttributes: true,
@@ -46,19 +59,26 @@ function html() {
     .pipe(dest("./dist"));
 }
 
+function size() {
+  return src(["./dist/**/*.css", "./dist/**/*.js"]).pipe(
+    sizereport({
+      gzip: true
+    })
+  );
+}
+
 function run(done) {
   browserSync.init({
     server: "./dist"
   });
 
   watch("static/**/*.css", css);
-  watch([
-    "dist/**/*.css",
-    "dist/**/*.js",
-    "dist/**/*.html"
-  ]).on("change", browserSync.reload);
+  watch(["dist/**/*.css", "dist/**/*.js", "dist/**/*.html"]).on(
+    "change",
+    browserSync.reload
+  );
   done();
 }
 
 exports.dev = series(css, run);
-exports.default = series(css, hashStyles, html);
+exports.default = series(css, hashStyles, html, size);
