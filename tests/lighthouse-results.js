69de9b25a02d51c184385ae0a76644fbae5e38b1
Aleš Roubíček <ales@roubicek.name>
2019-07-24 05:24:57 +0200

    Added Lighthouse tests

---
+++ tests/lighthouse-results.js
@@ -0,0 +1,25 @@
+import test from "ava";
+import results from "../results/root.report.json";
+
+const assertThreshold = ({ score }, threshold) => t =>
+  t.true(
+    score >= threshold,
+    `Score should be at least ${100 * threshold}%. Actual: ${100 * score}%`
+  );
+
+test(
+  "Lighthouse Accessibility",
+  assertThreshold(results.categories.accessibility, 0.75)
+);
+
+test(
+  "Lighthouse Best practices",
+  assertThreshold(results.categories["best-practices"], 0.75)
+);
+
+test(
+  "Lighthouse Performance",
+  assertThreshold(results.categories.performance, 0.75)
+);
+
+test("Lighthouse SEO", assertThreshold(results.categories.seo, 0.75));
