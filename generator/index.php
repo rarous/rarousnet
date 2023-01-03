46fbf0dc4e08d70fcbde72c5a0bbbe7b1db475d0
Aleš Roubíček <ales@roubicek.name>
2023-01-03 13:12:00 +0100

    formatting

--- generator/index.php
+++ generator/index.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 if (@!include __DIR__ . '/vendor/autoload.php') {
-	die('Install packages using `composer install`');
+  die('Install packages using `composer install`');
 }
 
 $contents = file_get_contents('php://stdin');
