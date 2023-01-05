2d1e9886bc02f086b1dca9f4fa25cc69d041dca9
Aleš Roubíček <ales@roubicek.name>
2023-01-05 15:25:10 +0100

    SFeed drop 11

--- generator/src/rarousnet/generator.clj
+++ generator/src/rarousnet/generator.clj
@@ -418,7 +418,7 @@
           articles
           (group-by :year)
           (sort-by first >)
-          (drop 9)
+          (drop 11)
           (map (fn [[year articles]]
                    {:year year
                     :articles (sort-by :timestamp > articles)})))
