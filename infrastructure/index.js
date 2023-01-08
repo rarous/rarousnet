7dbd89252e16426f4670a011f58dca83d9604ade
Aleš Roubíček <ales@roubicek.name>
2023-01-08 13:12:06 +0100

    yadayada

--- infrastructure/index.js
+++ infrastructure/index.js
@@ -113,7 +113,6 @@ new cloudflare.Record(`${domain}/dns-record-keybase`, {
 });
 
 const weblogNS = new cloudflare.WorkersKvNamespace("weblog-kv-ns", {
-  accountId: config.get("account_id"),
   title: "rarous-net-weblog",
 });
 
