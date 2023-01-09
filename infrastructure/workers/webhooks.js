2767065e74dd4c0a47f92f3d10fe42422731003a
Aleš Roubíček <ales@roubicek.name>
2023-01-09 20:08:21 +0100

    Webmentions webhook implementation

--- infrastructure/workers/webhooks.js
+++ infrastructure/workers/webhooks.js
@@ -2,12 +2,25 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
+async function getPayload(weblog, url) {
+  const payload = await weblog.get(url);
+  if (payload) return JSON.parse(payload);
+  return { webmentions: [] };
+}
+
+async function setPayload(weblog, url, payload) {
+  return weblog.set(url, JSON.stringify(payload));
+}
+
 app.post("/webhooks/webmentions", async (c) => {
   const { env, req } = c;
   const secret = env.WEBMENTIONS_WEBHOOK_SECRET;
   const body = await req.json();
   if (body.secret !== secret) return c.status(403);
-  console.log(body.post);
+  const { post, target } = body;
+  const payload = await getPayload(env.weblog, target);
+  payload.webmentions.push(post);
+  await setPayload(env.weblog, target, payload);
   return c.status(202);
 });
 
