41ff383be16519fe66f148ef7d3853130acfffe6
Aleš Roubíček <ales@roubicek.name>
2023-01-10 08:01:30 +0100

    Webmentions webhook support for deletion and updates

--- infrastructure/workers/webhooks.js
+++ infrastructure/workers/webhooks.js
@@ -18,9 +18,15 @@ app.post("/webhooks/webmentions", async (c) => {
     const secret = env.WEBMENTIONS_WEBHOOK_SECRET;
     const body = await req.json();
     if (body.secret !== secret) return c.status(403);
-    const { post, target } = body;
+    const { post, target, deleted } = body;
     const payload = await getPayload(env.weblog, target);
-    payload.webmentions.push(post);
+    const webmentions = new Map(payload.webmentions.map(x => [x.url, x]));
+    if (deleted) {
+      webmentions.delete(post.url);
+    } else {
+      webmentions.set(post.url, post);
+    }
+    payload.webmentions = Array.from(webmentions.values());
     await setPayload(env.weblog, target, payload);
     return c.status(202);
   } catch (err) {
