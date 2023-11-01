c4ced14f8acfcf8d2f7ed54a59d62538bdeebb88
Aleš Roubíček <ales@roubicek.name>
2023-11-01 16:58:28 +0100

    Change logging

--- functions/api/v1/weblog.js
+++ functions/api/v1/weblog.js
@@ -9,7 +9,7 @@
  */
 async function getDetail(weblog, url) {
   const payload = await weblog.get(url);
-  console.log(await weblog.list({ limit: 10 }));
+  console.log({ url, payload });
   if (payload) return JSON.parse(payload);
   return { webmentions: [] };
 }
