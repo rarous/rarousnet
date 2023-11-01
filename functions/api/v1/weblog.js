331a5d5231f281ab0ab50649a7c77bcd8469c9cd
Aleš Roubíček <ales@roubicek.name>
2023-11-01 16:10:49 +0100

    Use dir instead of log

--- functions/api/v1/weblog.js
+++ functions/api/v1/weblog.js
@@ -9,7 +9,7 @@
  */
 async function getDetail(weblog, url) {
   const payload = await weblog.get(url);
-  console.log(weblog);
+  console.dir(weblog);
   if (payload) return JSON.parse(payload);
   return { webmentions: [] };
 }
