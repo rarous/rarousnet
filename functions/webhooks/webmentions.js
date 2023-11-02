245546ba191103c2033ccf1170109ea7602a08cb
Aleš Roubíček <ales@roubicek.name>
2023-11-02 07:48:54 +0100

    Get JSON from KV

--- functions/webhooks/webmentions.js
+++ functions/webhooks/webmentions.js
@@ -9,8 +9,8 @@
  * @param {string} url
  */
 async function getDetail(weblog, url) {
-  const payload = await weblog.get(url, { type: "text" });
-  if (payload) return JSON.parse(payload);
+  const payload = await weblog.get(url, "json");
+  if (payload) return payload;
   return { webmentions: [] };
 }
 
