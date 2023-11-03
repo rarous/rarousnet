3e34982ea6b19bde76d05e10fe533ccd2ad8f510
Aleš Roubíček <ales@roubicek.name>
2023-11-03 21:55:08 +0100

    Return response with etag and content type

--- functions/files/[[catchall]].js
+++ functions/files/[[catchall]].js
@@ -12,5 +12,10 @@ export async function onRequestGet({ env, request }) {
   if (blob === null) {
     return new Response("Not found", { status: 404 });
   }
-  return new Response(blob.body);
+  return new Response(blob.body, {
+    headers: {
+      "content-type": blob.httpMetadata.contentType,
+      etag: blob.httpEtag,
+    },
+  });
 }
