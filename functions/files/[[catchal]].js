573b92d8b20e8bfe450a87fb386677f9c235ea8a
Aleš Roubíček <ales@roubicek.name>
2023-11-03 20:56:24 +0100

    Debugging proxy function

--- functions/files/[[catchal]].js
+++ functions/files/[[catchal]].js
@@ -5,8 +5,13 @@
 /**
  * @param {EventContext<Env>} context
  */
-export async function onRequestGet({ env, params }) {
+export async function onRequestGet({ env, params, request }) {
+  const { url } = request;
   const path = params.catchall;
+  console.log({ url, path, key: `files/${path}`, storage: env.storage });
   const blob = await env.storage.get(`files/${path}`);
+  if (blob === null) {
+    return new Response("Not found", { status: 404 });
+  }
   return new Response(blob.body());
 }
