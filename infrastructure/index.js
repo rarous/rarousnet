6a463156ae85e08444a80602a2aea0695062ebde
Aleš Roubíček <ales@roubicek.name>
2023-11-03 03:09:29 +0100

    Remoce S3 bucket

--- infrastructure/index.js
+++ infrastructure/index.js
@@ -50,61 +50,7 @@ new cloudflare.ZoneSettingsOverride(`${domain}/zone-settings`, {
   },
 });
 
-// TODO: migrate to R2 and proxy via function
-
-const bucket = new aws.s3.Bucket(`${domain}/bucket`, {
-  bucket: domain,
-  acl: "public-read",
-  website: {
-    indexDocument: "index.html",
-    errorDocument: "404.html",
-  },
-});
-
-new aws.s3.BucketPolicy(`${domain}/bucket-policy`, {
-  bucket: bucket.bucket,
-  policy: JSON.stringify({
-    Version: "2012-10-17",
-    Statement: [
-      {
-        Sid: "PublicReadGetObject",
-        Effect: "Allow",
-        Principal: {
-          AWS: "*",
-        },
-        Action: "s3:GetObject",
-        Resource: `arn:aws:s3:::${domain}/*`,
-        Condition: {
-          IpAddress: {
-            "aws:SourceIp": [
-              "2400:cb00::/32",
-              "2405:8100::/32",
-              "2405:b500::/32",
-              "2606:4700::/32",
-              "2803:f800::/32",
-              "2c0f:f248::/32",
-              "2a06:98c0::/29",
-              "103.21.244.0/22",
-              "103.22.200.0/22",
-              "103.31.4.0/22",
-              "104.16.0.0/12",
-              "108.162.192.0/18",
-              "131.0.72.0/22",
-              "141.101.64.0/18",
-              "162.158.0.0/15",
-              "172.64.0.0/13",
-              "173.245.48.0/20",
-              "188.114.96.0/20",
-              "190.93.240.0/20",
-              "197.234.240.0/22",
-              "198.41.128.0/17",
-            ],
-          },
-        },
-      },
-    ],
-  }),
-});
+// TODO: create R2 bucket and proxy via function
 
 new cloudflare.Record(`${domain}/dns-record-keybase`, {
   zoneId: zone.id,
@@ -169,10 +115,6 @@ const weblogPagesDomain = new cloudflare.PagesDomain("weblog-domain", {
 export const accountId = account.id;
 export const zoneId = zone.id;
 export const nameServers = zone.nameServers;
-export const bucketUri = bucket.bucket.apply((x) => `s3://${x}`);
-export const websiteTestUri = bucket.websiteEndpoint.apply(
-  (x) => `http://${x}`,
-);
 export const websiteUri = `https://${domain}`;
 export const weblogDomains = weblogPages.domains;
 export const weblogKvNsId = weblogNS.id;
