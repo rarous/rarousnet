3d83b3378d42f0414b82088aee22b4fa97b30141
Aleš Roubíček <ales@roubicek.name>
2023-11-04 20:13:32 +0100

    Youtube Texy handler

--- generator/index.php
+++ generator/index.php
@@ -6,6 +6,20 @@ if (@!include __DIR__ . '/vendor/autoload.php') {
   die('Install packages using `composer install`');
 }
 
+function youtubeHandler($invocation, $cmd, $args, $raw)
+{
+  if ($cmd == 'youtube') {
+    $videoId = $args[0];
+    $output = '<iframe width="560" height="315" class="youtube-video"
+      src="https://www.youtube-nocookie.com/embed/' . $videoId . '"
+      title="YouTube video player" frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen></iframe>';
+    return $invocation->texy->protect($output, Texy::CONTENT_MARKUP);
+  }
+  return $invocation->proceed();
+}
+
 $contents = file_get_contents('php://stdin');
 $articles = json_decode($contents, true);
 
@@ -21,7 +35,9 @@ $texy->allowedTags = true;
 $texy->headingModule->top = 3;
 $texy->headingModule->generateID = true;
 $texy->figureModule->class = 'image';
-$texy->imageModule->root = 'https://res.cloudinary.com/rarous/image/fetch/dpr_auto,f_auto/http://www.rarous.net/data/obrazky/';
+$texy->imageModule->root = 'https://res.cloudinary.com/rarous/image/fetch/dpr_auto,f_auto/https://www.rarous.net/data/obrazky/';
+
+$texy->addHandler('script', 'youtubeHandler');
 
 $output = array();
 foreach ($articles as $url => $text) {
