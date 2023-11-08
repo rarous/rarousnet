8720a8e33e23958cdc2f028ac58f949c563bd209
Aleš Roubíček <ales@roubicek.name>
2023-11-08 10:23:50 +0100

    Scripts for pushing webmentions from scraped data

---
+++ scripts/medium-likes.js
@@ -0,0 +1,42 @@
+import { parse } from "https://deno.land/std@0.205.0/flags/mod.ts";
+
+async function main({ token }) {
+  const {
+    default: [{ data }],
+  } = await import("./data/pagefind-likes.json", {
+    with: { type: "json" },
+  });
+  const target = "https://www.rarous.net/weblog/2023/10/23/pagefind.html";
+  const source = data.post.mediumUrl;
+  const { items } = data.post.voters;
+  const users = items.map((x) => x.user);
+  for (const user of users) {
+    const resp = await fetch("https://www.rarous.net/webhooks/webmentions", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        secret: token,
+        source,
+        target,
+        post: {
+          type: "entry",
+          author: {
+            name: user.name,
+            photo: `https://miro.medium.com/v2/resize:fill:80:80/${user.imageId}`,
+            url: user.hasSubdomain
+              ? `https://${user.username}.medium.com/`
+              : `https://medium.com/@${user.username}`,
+          },
+          url: `${source}#liked-by-${user.id}`,
+          published: "2015-11-08T03:38:09+00:00",
+          name: `like of ${source}`,
+          "like-of": source,
+          "wm-property": "like-of",
+        },
+      }),
+    });
+    console.log(resp);
+  }
+}
+
+await main(parse(Deno.args));
---
+++ scripts/twitter-likes.js
@@ -0,0 +1,42 @@
+import { parse } from "https://deno.land/std@0.205.0/flags/mod.ts";
+
+async function main({ token }) {
+  const {
+    default: { data },
+  } = await import("./data/pagefind-likes.json", {
+    with: { type: "json" },
+  });
+  const target = "https://www.rarous.net/weblog/2023/10/23/pagefind.html";
+  const source = "https://twitter.com/alesroubicek/status/1716417697607455226";
+  const { entries } = data.favoriters_timeline.timeline.instructions[0];
+  const users = entries
+    .filter((x) => x.content.entryType === "TimelineTimelineItem")
+    .map((x) => x.content.itemContent.user_results.result);
+  for (const { legacy: user, rest_id } of users) {
+    const resp = await fetch("https://www.rarous.net/webhooks/webmentions", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        secret: token,
+        source,
+        target,
+        post: {
+          type: "entry",
+          author: {
+            name: user.name,
+            photo: user.profile_image_url_https,
+            url: `https://twitter.com/${user.screen_name}`,
+          },
+          url: `${source}#favorited-by-${rest_id}`,
+          published: null,
+          name: `like of ${source}`,
+          "like-of": source,
+          "wm-property": "like-of",
+        },
+      }),
+    });
+    console.log(resp);
+  }
+}
+
+await main(parse(Deno.args));
