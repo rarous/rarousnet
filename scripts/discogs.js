6d7be4afa91e2e948f5c764683bd6bb94159a0c6
Aleš Roubíček <ales@roubicek.name>
2024-01-01 21:08:30 +0100

    Sort albums by artist and year

--- scripts/discogs.js
+++ scripts/discogs.js
@@ -4,10 +4,10 @@ async function getReleases(page, token) {
   const params = new URLSearchParams({
     page,
     per_page: 100,
-    sort: "artist"
+    sort: "artist",
   });
   const resp = await fetch(`https://api.discogs.com/users/rarous/collection/folders/1/releases?${params}`, {
-    headers: { "Authorization": `Discogs token=${token}` }
+    headers: { "Authorization": `Discogs token=${token}` },
   });
   return resp.json();
 }
@@ -20,10 +20,10 @@ async function searchAlbumOnSpotify(q, spotifyToken) {
   const params = new URLSearchParams({
     q,
     type: "album,track",
-    limit: 1
+    limit: 1,
   });
   const resp = await fetch(`https://api.spotify.com/v1/search?${params}`, {
-    headers: { Authorization: `Bearer ${spotifyToken}` }
+    headers: { Authorization: `Bearer ${spotifyToken}` },
   });
   const { albums } = await resp.json();
   return albums;
@@ -42,14 +42,23 @@ async function main({ token, spotifyToken }) {
       title: x.title,
       image: x.cover_image,
       year: x.year,
-      artist: { id: x.artists[0].id, name: cleanArtistName(x.artists[0].name) }
+      artist: { id: x.artists[0].id, name: cleanArtistName(x.artists[0].name) },
     }));
     result.push(...items);
     stop = pagination.pages === page++;
   } while (!stop);
 
-  for (const item of result) {
-    let albums = await searchAlbumOnSpotify(`artist:${item.artist.name} album:${item.title} year:${item.year}`, spotifyToken);
+  function byArtistAndYear(a, b) {
+    const comparison = a.artist.name.localeCompare(b.artist.name);
+    if (comparison !== 0) return comparison;
+    return a.year - b.year;
+  }
+
+  for (const item of result.sort(byArtistAndYear)) {
+    let albums = await searchAlbumOnSpotify(
+      `artist:${item.artist.name} album:${item.title} year:${item.year}`,
+      spotifyToken,
+    );
     if (!albums.items.length) {
       // if we don't find album for exact release year, try to search for any year
       albums = await searchAlbumOnSpotify(`artist:${item.artist.name} album:${item.title}`, spotifyToken);
