4f55cb4f72191067ff783819d53c5d6e3985d7a6
Aleš Roubíček <ales@roubicek.name>
2024-01-01 20:41:31 +0100

    Vinyl collection with Spotify player

--- scripts/discogs.js
+++ scripts/discogs.js
@@ -12,7 +12,24 @@ async function getReleases(page, token) {
   return resp.json();
 }
 
-async function main({ token }) {
+function cleanArtistName(name) {
+  return name.replace(/\s\(\d+\)$/, "");
+}
+
+async function searchAlbumOnSpotify(q, spotifyToken) {
+  const params = new URLSearchParams({
+    q,
+    type: "album,track",
+    limit: 1
+  });
+  const resp = await fetch(`https://api.spotify.com/v1/search?${params}`, {
+    headers: { Authorization: `Bearer ${spotifyToken}` }
+  });
+  const { albums } = await resp.json();
+  return albums;
+}
+
+async function main({ token, spotifyToken }) {
   const result = [];
 
   let page = 1;
@@ -25,15 +42,24 @@ async function main({ token }) {
       title: x.title,
       image: x.cover_image,
       year: x.year,
-      artist: { id: x.artists[0].id, name: x.artists[0].name }
+      artist: { id: x.artists[0].id, name: cleanArtistName(x.artists[0].name) }
     }));
     result.push(...items);
     stop = pagination.pages === page++;
   } while (!stop);
+
+  for (const item of result) {
+    let albums = await searchAlbumOnSpotify(`artist:${item.artist.name} album:${item.title} year:${item.year}`, spotifyToken);
+    if (!albums.items.length) {
+      // if we don't find album for exact release year, try to search for any year
+      albums = await searchAlbumOnSpotify(`artist:${item.artist.name} album:${item.title}`, spotifyToken);
+    }
+    item.spotifyUri = albums.items?.[0]?.uri;
+  }
 
   console.log(JSON.stringify(result));
 }
 
 await main(parse(Deno.args));
 
-// deno run --allow-net=api.discogs.com discogs.js --token="$(op read 'op://Private/Discogs/API/Access Token')"
+// deno run --allow-net=api.discogs.com,api.spotify.com discogs.js --token="$(op read 'op://Private/Discogs/API/Access Token')"
