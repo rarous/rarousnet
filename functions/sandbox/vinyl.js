f335b142457328199fdcdbae2927dd8f4362ba3d
Aleš Roubíček <ales@roubicek.name>
2024-01-02 20:26:40 +0100

    inline web component to function

--- functions/sandbox/vinyl.js
+++ functions/sandbox/vinyl.js
@@ -1,13 +1,39 @@
 import { parseHTML } from "linkedom/worker";
-import { Discogs } from "../../website/assets/esm/discogs.js";
 
 /**
  * @typedef {import("../env.d.ts").Env} Env
  */
 
-function registerGlobals(global) {
-  globalThis.HTMLElement = global.HTMLElement;
-  return global;
+/**
+ * @param {Element} section
+ * @param {Array} items
+ */
+function itemsCounter(section, items) {
+  const counter = section.querySelector("data");
+  counter.value = items.length;
+  counter.textContent = items.length;
+  return counter;
+}
+
+/**
+ * @param {Element} section
+ * @param {Element} template
+ * @param {Array} items
+ * @param {Function} applyTemplate
+ */
+function injectItems(section, template, items, applyTemplate) {
+  if (!items?.length) {
+    return section.remove();
+  }
+  itemsCounter(section, items);
+  const list = section.querySelector(".items");
+  const listItems = document.createDocumentFragment();
+  for (const item of items) {
+    const { content } = template.cloneNode(true);
+    applyTemplate(content, item, section);
+    listItems.appendChild(content);
+  }
+  list.replaceChildren(listItems);
 }
 
 /**
@@ -16,7 +42,48 @@ function registerGlobals(global) {
 export async function onRequestGet({ env }) {
   const resp = await env.ASSETS.fetch("/kolekce/vinyly.html");
   const html = await resp.text();
-  const { document, customElements } = registerGlobals(parseHTML(html));
+  const { document, customElements, HTMLElement } = parseHTML(html);
+
+  class Discogs extends HTMLElement {
+    constructor() {
+      super();
+    }
+
+    set data(albums) {
+      if (!albums?.length) return;
+
+      const template = this.querySelector("template");
+      const collection = this.querySelector("section");
+
+      function itemTemplate(content, item) {
+        const link = content.querySelector("a");
+        link.href = link.href + item.id;
+
+        const img = content.querySelector("img");
+        img.src = `https://res.cloudinary.com/rarous/image/fetch/dpr_auto,f_auto/${item.image}`;
+        img.alt = `Obal desky ${item.artist.name} - ${item.title} (${item.year})`;
+
+        const name = content.querySelector("[property=name]");
+        name.textContent = item.title;
+
+        const byArtist = content.querySelector("[property=byArtist]");
+        byArtist.textContent = item.artist.name;
+
+        const copyrightYear = content.querySelector("[property=copyrightYear]");
+        copyrightYear.textContent = item.year;
+
+        const button = content.querySelector("button");
+        if (!item.spotifyUri) {
+          button.remove();
+        } else {
+          button.dataset.spotifyUri = item.spotifyUri;
+        }
+      }
+
+      injectItems(collection, template, albums, itemTemplate);
+    }
+  }
+
   customElements.define("rarous-discogs", Discogs);
   const discogs = document.querySelector("rarous-discogs");
   discogs.data = await env.weblog.get("/kolekce/vinyly", "json");
