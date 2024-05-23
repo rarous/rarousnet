34a26572c3715766f2bbd5fc0b3987505b1b4ba6
Aleš Roubíček <ales@roubicek.name>
2024-05-23 16:10:38 +0200

    Fix default values

--- functions/api/v1/qr.js
+++ functions/api/v1/qr.js
@@ -1,13 +1,19 @@
 import { getSVG } from "qreator/lib/svg";
 
+/**
+ * Generates QR code for given `text`. `color`, `bgColor` and `borderRadius` can be changed via GET parameters.
+ * @param {EventContext<Env>} context
+ * @returns {Promise<Response>}
+ */
+
 export async function onRequestGet({ request }) {
   const { searchParams } = new URL(request.url);
   const text = searchParams.get("t") ?? searchParams.get("text");
   if (!text) return new Response("Missing required parameter: text", { status: 400 });
 
-  const color = searchParams.get("c") ?? searchParams.get("color");
-  const bgColor = searchParams.get("bg") ?? searchParams.get("bgColor");
-  const borderRadius = searchParams.get("br") ?? searchParams.get("borderRadius");
+  const color = searchParams.get("c") ?? searchParams.get("color") ?? "#000000";
+  const bgColor = searchParams.get("bg") ?? searchParams.get("bgColor") ?? "#ffffff";
+  const borderRadius = parseInt(searchParams.get("br") ?? searchParams.get("borderRadius") ?? "0");
   const buffer = await getSVG(text, { color, bgColor, borderRadius });
   return new Response(buffer, {
     status: 200,
