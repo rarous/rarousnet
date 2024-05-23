e71c6fc822fcf2dd2e5b5e756c3e116a94f55dea
Aleš Roubíček <ales@roubicek.name>
2024-05-23 14:19:36 +0200

    Set size of the QR image

--- functions/api/v1/qr.js
+++ functions/api/v1/qr.js
@@ -3,8 +3,10 @@ import { imageSync } from "qr-image";
 export async function onRequestGet({ request }) {
   const { searchParams } = new URL(request.url);
   const text = searchParams.get("text");
-  if (!text) return new Response("Missing parameter text", { status: 400 });
-  const buffer = imageSync(text, { type: "png" });
+  const size = parseInt(searchParams.get("size") ?? "200");
+  if (!text) return new Response("Missing required parameter: text", { status: 400 });
+
+  const buffer = imageSync(text, { type: "png", size });
   return new Response(buffer, {
     headers: { "Content-Type": "image/png" },
   });
