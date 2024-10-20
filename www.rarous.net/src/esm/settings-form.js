49f6f846f7e4f9cb17948eb1bd926dd0f1dea8c5
Aleš Roubíček <ales@roubicek.name>
2024-10-20 13:32:46 +0200

    Select stored values in form after reload, refactoring and docs

--- www.rarous.net/src/esm/settings-form.js
+++ www.rarous.net/src/esm/settings-form.js
@@ -1,4 +1,32 @@
 /**
+ * @param {HTMLElement} root
+ * @param {IterableIterator<[key: string, value: string]>} data
+ */
+function updateSettings(root, data) {
+  for (const [key, value] of data) {
+    root.dataset[key] = value;
+  }
+}
+
+/**
+ * @param {HTMLFormElement} form
+ * @param {IterableIterator<[key: string, value: string]>} data
+ */
+function updateForm(form, data) {
+  for (const [key, value] of data) {
+    form[key].value = value;
+  }
+}
+
+/**
+ * This component stores user preferences in `localStorage`
+ * and applies them when changed or when user comes back.
+ * Preferences are applied as data attributes on the root element.
+ * You can then use them in CSS or scripts.
+ *
+ * This component is progressive enhancement wrapper
+ * and expects a form element with associated inputs inside.
+ *
  * @param {Window} globalScope
  * @return {typeof SettingsForm}
  */
@@ -12,19 +40,16 @@ export function defSettingsForm({ HTMLElement, customElements, document }) {
     connectedCallback() {
       const root = document.documentElement;
       const form = this.querySelector("form");
+
       form.addEventListener("submit", e => {
         const data = new FormData(e.target);
-        for (const [key, value] of data) {
-          console.log([key, value]);
-          root.dataset[key] = value;
-        }
+        updateSettings(root, data);
         localStorage.setItem("settings", JSON.stringify(Object.fromEntries(data)));
       });
 
-      const settings = JSON.parse(localStorage.getItem("settings") ?? "{}");
-      for (const [key, value] of Object.entries(settings)) {
-        root.dataset[key] = value;
-      }
+      const settings = Object.entries(JSON.parse(localStorage.getItem("settings") ?? "{}"));
+      updateSettings(root, settings);
+      updateForm(form, settings);
     }
   }
 
