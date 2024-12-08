fd39809bd44f9a8563ec4b3ad0e23088c012dfe1
Aleš Roubíček <ales@roubicek.name>
2024-12-08 12:07:23 +0100

    Sort articles in API

--- www.rarous.net/functions/api/v1/w3b/index.js
+++ www.rarous.net/functions/api/v1/w3b/index.js
@@ -2,6 +2,7 @@
  * @param {EventContext<Env>} context
  */
 export async function onRequestGet({ env }) {
-  const result = await env.w3b.get("/latest", "json");
+  const data = await env.w3b.get("/latest", "json");
+  const result = Object.values(data).sort((a, b) => -1 * a.entry.published.localeCompare(b.entry.published));
   return Response.json(result);
 }
