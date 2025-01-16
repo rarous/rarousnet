e8b577d4c61f73eb8df123c2de5129f60e7df15d
Aleš Roubíček <ales@roubicek.name>
2025-01-16 11:14:15 +0100

    Extend beacon support for unknown stats

--- www.rarous.net/functions/api/v1/w3b/beacon.js
+++ www.rarous.net/functions/api/v1/w3b/beacon.js
@@ -5,7 +5,8 @@ export async function onRequestPost({ request, env }) {
   const form = await request.formData();
   const { url, event } = Object.fromEntries(form);
   const latest = await env.w3b.get("/latest", "json");
-  latest[url].stats[event] += 1;
+  const { stats } = latest[url];
+  stats[event] = (stats[event] ?? 0) + 1;
   await env.w3b.put("/latest", JSON.stringify(latest));
   return new Response(null, { status: 202 });
 }
