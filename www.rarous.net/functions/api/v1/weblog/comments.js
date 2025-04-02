a607232b059eec519c94cf814eef6a30c6b1bd65
Aleš Roubíček <ales@roubicek.name>
2025-04-02 15:28:05 +0200

    More anti-spam work

--- www.rarous.net/functions/api/v1/weblog/comments.js
+++ www.rarous.net/functions/api/v1/weblog/comments.js
@@ -28,73 +28,73 @@ async function getDetail(weblog, url) {
 export const onRequestPost = [
   async context => turnstilePlugin({ secret: context.env.TURNSTILE_SECRET_KEY })(context),
   async context => {
-    try {
-      const now = new Date();
-      const { env, request } = context;
-      const url = new URL(request.url);
-      const target = url.searchParams.get("url");
-      if (target.startsWith("https:/www.rarous.net")) {
-        // spam with broken URL
-        return new Response(null, { status: 451 });
-      }
-
-      const [detail, comment] = await Promise.all([getDetail(env.weblog, target), request.formData()]);
-      // TODO: validate comment
-      if (/^\p{Script=Cyrillic}+$/u.test(comment.get("text"))) {
-        // russian spam
-        return new Response(null, { status: 451 });
-      }
-      const comments = detail?.comments ?? [];
-      const references = comments
-        .filter(x => x.isEnabled)
-        .map((x, i) => [
-          i.toString(),
-          {
-            link: `#komentar-${new Date(x.created).valueOf()}`,
-            label: `#${i + 1} @${x.author.name}`,
-          },
-        ]);
-      const textResult = processText(comment.get("text"), references);
-      const created = now.toISOString();
-      const author = {
-        name: comment.get("name"),
-        email: comment.get("email"),
-        web: comment.get("web"),
-      };
-      const text = await textResult;
-      const isEnabled = !(
-        text.includes("Coinbase") ||
-        text.includes(".ru/") ||
-        text.includes(".su/") ||
-        author.web?.includes(".ru")
-      ); // TODO: check for spam, hate etc. -> isEnabled = false;
-      const insert = { author, created, text, isEnabled };
-      comments.push(insert);
-
-      const upsert = Object.assign({}, detail, { comments });
-      await env.weblog.put(target, JSON.stringify(upsert));
-
-      const lastComments = (await env.weblog.get("/weblog/comments", "json")) ?? [];
-      lastComments.push(
-        Object.assign(
-          {
-            href: `${target}#komentar-${now.valueOf()}`,
-            article: { title: comment.get("article-title") ?? "" },
-          },
-          insert,
-        ),
-      );
-      if (lastComments.length > 10) lastComments.shift();
-      await env.weblog.put("/weblog/comments", JSON.stringify(lastComments));
-
-      const accept = request.headers.get("accept");
-      if (accept === "application/json") {
-        return Response.json(insert);
-      }
-      return Response.redirect(`${target}#komentar-${now.valueOf()}`);
-    } catch (err) {
-      console.error(err);
-      return new Response(null, { status: 500 });
+    const now = new Date();
+    const { env, request } = context;
+    if (!request.cf.botManagement.jsDetection.passed) {
+      // CF classified bot, skip
+      return new Response(null, { status: 451 });
     }
+
+    const url = new URL(request.url);
+    const target = url.searchParams.get("url");
+    if (target.startsWith("https:/www.rarous.net")) {
+      // spam with broken URL
+      return new Response(null, { status: 451 });
+    }
+
+    const [detail, comment] = await Promise.all([getDetail(env.weblog, target), request.formData()]);
+    // TODO: validate comment
+    if (/^\p{Script=Cyrillic}+$/u.test(comment.get("text"))) {
+      // russian spam
+      return new Response(null, { status: 451 });
+    }
+    const comments = detail?.comments ?? [];
+    const references = comments
+      .filter(x => x.isEnabled)
+      .map((x, i) => [
+        i.toString(),
+        {
+          link: `#komentar-${new Date(x.created).valueOf()}`,
+          label: `#${i + 1} @${x.author.name}`,
+        },
+      ]);
+    const textResult = processText(comment.get("text"), references);
+    const created = now.toISOString();
+    const author = {
+      name: comment.get("name"),
+      email: comment.get("email"),
+      web: comment.get("web"),
+    };
+    const text = await textResult;
+    const isEnabled = !(
+      text.includes("Coinbase") ||
+      text.includes(".ru/") ||
+      text.includes(".su/") ||
+      author.web?.includes(".ru")
+    ); // TODO: check for spam, hate etc. -> isEnabled = false;
+    const insert = { author, created, text, isEnabled };
+    comments.push(insert);
+
+    const upsert = Object.assign({}, detail, { comments });
+    await env.weblog.put(target, JSON.stringify(upsert));
+
+    const lastComments = (await env.weblog.get("/weblog/comments", "json")) ?? [];
+    lastComments.push(
+      Object.assign(
+        {
+          href: `${target}#komentar-${now.valueOf()}`,
+          article: { title: comment.get("article-title") ?? "" },
+        },
+        insert,
+      ),
+    );
+    if (lastComments.length > 10) lastComments.shift();
+    await env.weblog.put("/weblog/comments", JSON.stringify(lastComments));
+
+    const accept = request.headers.get("accept");
+    if (accept === "application/json") {
+      return Response.json(insert);
+    }
+    return Response.redirect(`${target}#komentar-${now.valueOf()}`);
   },
 ];
