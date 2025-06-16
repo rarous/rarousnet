8964d2a1b12d15724e45fa37dab71152010a83bb
Aleš Roubíček <ales@roubicek.name>
2025-06-16 07:54:20 +0200

    Bumped Blendid

--- www.rarous.net/config/task-config.mjs
+++ www.rarous.net/config/task-config.mjs
@@ -1,4 +1,3 @@
-import { getPathConfig } from "@hckr_/blendid/lib/getPathConfig.mjs";
 import { texyTypography } from "@hckr_/blendid/lib/texy.mjs";
 import OpenProps from "open-props";
 import jitProps from "postcss-jit-props";
@@ -7,65 +6,66 @@ import { GryphoonRegistry } from "./gryphoon-registry.mjs";
 /** @typedef {import("@types/nunjucks").Environment} Environment */
 /** @typedef {import("@types/gulp").Gulp} Gulp */
 
-const pathConfig = await getPathConfig();
+export default function (pathConfig, mode, verbose) {
+  return {
+    images: true,
+    cloudflare: true,
+    cloudinary: false,
+    fonts: true,
+    svgSprite: true,
+    esbuild: true,
 
-export default {
-  images: true,
-  cloudflare: true,
-  cloudinary: false,
-  fonts: true,
-  svgSprite: true,
-  esbuild: true,
-
-  static: {
-    srcConfig: {
-      encoding: false,
+    static: {
+      srcConfig: {
+        encoding: false,
+      },
     },
-  },
 
-  stylesheets: {
-    postcss: {
-      plugins: [jitProps(OpenProps)],
+    stylesheets: {
+      postcss: {
+        plugins: [jitProps(OpenProps)],
+      },
     },
-  },
 
-  html: {
-    markedExtensions: [texyTypography("cs")],
-    data: { collections: [] },
-    htmlmin: {
-      collapseBooleanAttributes: true,
-      collapseWhitespace: true,
-      preserveLineBreaks: true,
-      minifyJS: true,
-      removeAttributeQuotes: true,
-      removeComments: true,
-      removeOptionalTags: true,
-      removeRedundantAttributes: true,
-      removeScriptTypeAttributes: true,
-      removeStyleLinkTypeAttributes: true,
+    html: {
+      markedExtensions: [texyTypography("cs")],
+      data: { collections: [] },
+      htmlmin: {
+        collapseBooleanAttributes: true,
+        collapseWhitespace: true,
+        preserveLineBreaks: true,
+        // TODO: reimplement as function with PostCSS
+        minifyJS: true,
+        removeAttributeQuotes: true,
+        removeComments: true,
+        removeOptionalTags: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+      },
     },
-  },
 
-  vite: {
-    server: { port: 3001 },
-    browser: "firefox developer edition",
-    browserArgs: "--ignore-certificate-errors --allow-insecure-localhost",
-  },
-
-  production: {
-    rev: {
-      exclude: ["_headers", "_redirects", "weblog/articles.rss", "weblog/sitemap.xml"],
+    vite: {
+      server: { port: 3001 },
+      browser: "firefox developer edition",
+      browserArgs: "--ignore-certificate-errors --allow-insecure-localhost",
     },
-  },
 
-  registries: [new GryphoonRegistry({}, pathConfig)],
-
-  additionalTasks: {
-    development: {
-      posthtml: ["generate-content"],
-    },
     production: {
-      posthtml: ["generate-content", "upload-cards"],
+      rev: {
+        exclude: ["_headers", "_redirects", "weblog/articles.rss", "weblog/sitemap.xml"],
+      },
     },
-  },
-};
+
+    registries: [new GryphoonRegistry({}, pathConfig, mode)],
+
+    additionalTasks: {
+      development: {
+        posthtml: ["generate-content"],
+      },
+      production: {
+        posthtml: ["generate-content", "upload-cards"],
+      },
+    },
+  };
+}
