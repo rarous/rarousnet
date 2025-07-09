0f1936052d85a829e3766e4b6fec85bbec092297
Aleš Roubíček <ales@roubicek.name>
2025-07-09 08:28:37 +0200

    Do not try to fetch fallback for API requests.

--- www.rarous.net/functions/_middleware.js
+++ www.rarous.net/functions/_middleware.js
@@ -8,8 +8,20 @@ export async function rollbar(context) {
   return rollbarPlugin({ token: context.env.ROLLBAR_TOKEN })(context);
 }
 
-function getKeyWithHtml(key) {
-  return `${key}${key.endsWith("/") ? "" : "/"}index.html`;
+function getKeyWithHtml(key, indexFile = "index.html") {
+  return `${key}${key.endsWith("/") ? "" : "/"}${indexFile}`;
+}
+
+function* fallbackKey(key) {
+  yield key;
+  yield getKeyWithHtml(key);
+  yield getKeyWithHtml(key, "default.htm");
+}
+
+async function* fallbacks(env, key) {
+  for (const k of fallbackKey(key)) {
+    yield await env.storage.get(key);
+  }
 }
 
 /**
@@ -20,20 +32,17 @@ export async function r2Fallback({ env, next, request }) {
   const resp = await next();
   if (resp.status === 200) return resp;
 
+  // Short-circuit for API request, there is no need to search for fallback
+  if (new URLPattern({ pathname: "/api/*" }).test(request.url)) return resp;
+
   // Fallback to R2 bucket
   const url = new URL(request.url);
   const key = url.pathname.substring(1);
-  const blob = await env.storage.get(key);
-  if (blob) {
+  for await (const blob of fallbacks(env, key)) {
+    if (!blob) continue;
     const headers = new Headers();
     blob.writeHttpMetadata(headers);
     return new Response(blob.body, { headers });
-  }
-  const blobHtml = await env.storage.get(getKeyWithHtml(key));
-  if (blobHtml) {
-    const headers = new Headers();
-    blobHtml.writeHttpMetadata(headers);
-    return new Response(blobHtml.body, { headers });
   }
 
   // Let Cloudflare decide what to do - most likely 404
