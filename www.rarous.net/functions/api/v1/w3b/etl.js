5e4e1a0a359166d605d4d18c5aa93736611ed6d8
Aleš Roubíček <ales@roubicek.name>
2025-10-24 12:11:34 +0200

    Fix URL update logic, its deeper

--- www.rarous.net/functions/api/v1/w3b/etl.js
+++ www.rarous.net/functions/api/v1/w3b/etl.js
@@ -5,13 +5,13 @@
 export async function onRequestGet({ env }) {
   const index = await env.w3b.get("/latest", "json");
 
-  for (const entry of Object.values(index)) {
+  for (const entry of Object.values(index).map(x => Object.assign(x, { entry: Object.assign(x.entry, { link: undefined, url: x.entry.link }) }))) {
     const resp = await fetch(`https://gryphoon.work/api/v1/w3blogy.cz/articles/share`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(Object.assign(entry, { link: undefined, url: entry.link }))
+      body: JSON.stringify(entry)
     });
   }
   return new Response();
